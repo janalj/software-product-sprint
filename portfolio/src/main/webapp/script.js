df1fc0a8b719f73af406c76d3c1d1f3a2f81b8bb
Luying Jiang <ljiang@sps-program.com>
2022-06-03 10:09:49 +0000

    Add type writer effect to caption

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,28 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+// Play caption 
+
+const title = document.getElementById("caption");
+const text = "//Aspiring Software Engineer...";
+
+let index = 0;
+
+const randomSpeed = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const play = () => {
+  title.innerHTML = text.slice(0, index);
+  index++;
+
+  if (index > text.length) {
+    index = 0;
+  }
+
+  clearInterval(timer);
+  timer = setInterval(play, randomSpeed(70, 300));
+};
+
+let timer = setInterval(play, 300);
